d93c0bc277894990cb1115d17f605205efb4f576
kkebuka <kkebuka@gmail.com>
2024-08-20 22:15:37 +0100

    other assets added

---
+++ src/landingpage/components/Solution.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Solution() {
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>Our solution cuts across </h2>
+          <h2>African countries</h2>
+        </div>
+        <p>
+          Businessses all over the world can use the rentwise AI rental solution
+          to improve their ability to make well informed decisions when it comes
+          to renting or buying real estate. procedures for authentication and
+          verification. no matter where your activities are located, our
+          commited collegues throughout the world are prepared to support you
+          and your clients.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Solution;
