6bf27bd138c18f15b83a65c9493f8d9b559a91e3
kkebuka <kkebuka@gmail.com>
2024-08-22 12:35:16 +0100

    wise report update

--- src/landingpage/components/WiseReport.jsx
+++ src/landingpage/components/WiseReport.jsx
@@ -7,6 +7,72 @@ function WiseReport() {
         <h2>
           Heard about the <span>Wise Report??</span>
         </h2>
+        <div>
+          <div>
+            <h3>
+              Just before you make that payment,{" "}
+              <a href="#    ">Download the wise report!</a>
+            </h3>
+            <p>
+              Your trusted gateway to seamless and stress free home rentals in
+              Lagos{" "}
+            </p>
+            <h4>
+              The Wise Report offers a comprehensive resource that equips both
+              landlords and prospective tenants with vital information—from
+              detailed property insights and landlord profiles to neighborhood
+              analysis, agent details, and reviews from previous tenants. This
+              empowers landlords to attract well-informed tenants while helping
+              tenants make confident, informed decisions.
+            </h4>
+            <button>Download WiseReport</button>
+          </div>
+          <div>
+            <div>
+              <h3>THE WISE REPORT</h3>
+              <h3>AUG 2024</h3>
+            </div>
+            <div>
+              <h3>INTRODUCTION</h3>
+              <div>
+                <div>
+                  <p>
+                    Wise Report provides a comprehensive report that includes
+                    property details, landlord information, neighbourhood
+                    insights, agent information, and previous tenant reviews,
+                    empowering you to make informed decisions.
+                  </p>
+                </div>
+                <div>
+                  <img src="" alt="" />
+                </div>
+              </div>
+            </div>
+            <div>
+              <h3>REPORT STRUCTURE</h3>
+              <div>
+                <div>
+                  <p>
+                    Wise Report provides a comprehensive report that includes
+                    property details, landlord information, neighbourhood
+                    insights, agent information, and previous tenant reviews,
+                    empowering you to make informed decisions.
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    Wise Report provides a comprehensive report that includes
+                    property details, landlord information, neighbourhood
+                    insights, agent information, and previous{" "}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h3>REPORT FEATURES</h3>
+            </div>
+          </div>
+        </div>
         <div></div>
       </div>
     </div>
