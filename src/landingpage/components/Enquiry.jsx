d36a409180fa9077b0dae98f1cfc1c59dae34984
kkebuka <kkebuka@gmail.com>
2024-08-25 20:10:39 +0100

    minor changes

--- src/landingpage/components/Enquiry.jsx
+++ src/landingpage/components/Enquiry.jsx
@@ -14,13 +14,89 @@ function Enquiry() {
             euismod. Risus etiam purus donec neque.{" "}
           </p>
         </div>
-        <div>
-          <form action="#">
-            <label htmlFor="inquiry">dsccewce</label>
-            <select name="Inquiry Type" id="inquiry">
-              <option value="test">test</option>
-              <option value="test1">test1</option>
+        <div className="">
+          <form action="#" className="p-4">
+            <label htmlFor="inquiry">Inquiry</label>
+            <select className="" name="Inquiry Type" id="inquiry">
+              <option value="" className="hidden">
+                Property Type
+              </option>
+              <option value="purchase">Purchase</option>
+              <option value="rent">Rent</option>
+              <option value="sell">Sell</option>
+              <option value="mortage">Mortage</option>
+              <option value="evaluation">Evaluation</option>
             </select>
+
+            <label htmlFor="inquiry">Inquiry</label>
+            <select className="block" name="Inquiry Type" id="inquiry">
+              <option value="" className="hidden">
+                I am a
+              </option>
+              <option value="propertyOwner">Property Owner</option>
+              <option value="RealEstateAgent">Real Estate Agent</option>
+            </select>
+
+            <div>
+              <input type="text" placeholder="First Name" />
+
+              <input type="text" placeholder="Last Name" />
+            </div>
+            <div>
+              <input type="text" placeholder="Email Address" />
+            </div>
+            <label htmlFor="location">Location</label>
+            <select className="" name="location Type" id="location">
+              <option value="" className="hidden">
+                Select
+              </option>
+              <option value="">All Cities in Lagos</option>
+              <option value="lagosIsland">Lagos Island</option>
+              <option value="victoriaIsland">Victoria Island</option>
+              <option value="lekki">Lekki</option>
+              <option value="ajah">Ajah</option>
+              <option value="surulere">Surulere</option>
+            </select>
+
+            <label htmlFor="property">Property</label>
+            <select className="" name="property Type" id="property">
+              <option value="" className="hidden">
+                Property Type
+              </option>
+              <optgroup
+                label="Commercial"
+                className="text-dropDowmcolor font-normal"
+              >
+                <option
+                  value="office"
+                  className="text-dropDowmcolor font-normal"
+                >
+                  Office
+                </option>
+                <option value="shop" className="text-dropDowmcolor font-normal">
+                  Shop
+                </option>
+              </optgroup>
+              <optgroup
+                label="Resedential"
+                className="text-dropDowmcolor font-normal"
+              >
+                <option value="apartment">Apartment</option>
+                <option value="singleFamilyHome">Single Family Home</option>
+                <option value="multiFamilyHome">Multi Family Home</option>
+              </optgroup>
+            </select>
+
+            <div>
+              <input type="text" placeholder="Max Price" />
+              <input type="text" placeholder="Minimum size (Sq Ft)" />
+            </div>
+            <div>
+              <input type="text" placeholder="Number of beds" />
+              <input type="text" placeholder="Number of baths" />
+            </div>
+
+            <button>Submit</button>
           </form>
         </div>
       </div>
