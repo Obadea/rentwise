072e4f825526278a17f2a438c32aa614965c8fe0
kkebuka <kkebuka@gmail.com>
2024-08-30 13:27:47 +0100

    minor change

--- src/landingpage/components/Solution.jsx
+++ src/landingpage/components/Solution.jsx
@@ -7,9 +7,9 @@ function Solution() {
     <div className="bg-customdark text-white px-6 lg:px-20 py-16">
       <div className="flex flex-col lg:flex-row gap-2">
         <div className="basis-[65%] px-0 lg:px-12 py-6">
-          <div>
-            <h2 className="text-4xl">Our solution cuts across </h2>
-            <h2 className="text-4xl">African countries</h2>
+          <div className="text-center lg:text-start">
+            <h2 className="text-4xl inline-block">Our solution cuts across </h2>
+            <h2 className="text-4xl inline lg"> African countries</h2>
           </div>
           <p className="text-base text-gray-400 my-6">
             Businessses all over the world can use the rentwise AI rental
@@ -20,9 +20,11 @@ function Solution() {
             support you and your clients.
           </p>
           <img src={country} alt="" />
-          <button className="mt-6 px-4 py-3 w-full lg:w-auto rounded-lg bg-customDownloadBlue border border-white">
-            Learn more
-          </button>
+          <div>
+            <button className="mt-6 px-4 py-3  w-full  lg:w-auto rounded-lg bg-customDownloadBlue border border-white">
+              Learn more
+            </button>
+          </div>
         </div>
         <div className="basis-[30%]">
           <img src={continent} alt="" className="h-full w-full px-4 lg:px-0" />
