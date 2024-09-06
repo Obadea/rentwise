943bd02a4f1cf1f09406cd81d816f05b77a52214
kkebuka <kkebuka@gmail.com>
2024-09-06 19:24:30 +0100

    minor issue resolve

---
+++ src/signinpage/SignInPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SignInPage() {
+  return (
+    <div>
+      <div>
+        <h2></h2>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default SignInPage;
