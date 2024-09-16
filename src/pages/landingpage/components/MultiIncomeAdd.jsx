f527ab02afb6be91c5b8da209a6482e6e045a382
kkebuka <kkebuka@gmail.com>
2024-09-16 19:04:42 +0100

    multi income component created

---
+++ src/pages/landingpage/components/MultiIncomeAdd.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MultiIncomeAdd() {
+  return <div></div>;
+}
+
+export default MultiIncomeAdd;
--- src/pages/landingpage/components/SearchBetter.jsx
+++ src/pages/landingpage/components/SearchBetter.jsx
@@ -4,12 +4,51 @@ import binoculars from "../../../assets/binoculars.png";
 
 function SearchBetter() {
   return (
-    <div>
+    <div className="rounded-3xl">
       <div>
         <CancelPresentationIcon />
       </div>
       <div>
-        <img src={binoculars} alt="" />
+        <div className="bg-textcolor rounded-full">
+          <img src={binoculars} alt="" />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <h2 className="font-bold text-3xl text-customNameBlack">
+            Search better
+          </h2>
+          <p className="font-normal text-base bg-customStreetcolor">
+            Search better using the household income rule. Household income is
+            the total money earned by all members of a household. It includes
+            money from jobs, business, investments, rent, and any government
+            help.
+          </p>
+          <h3 className="font-bold text-sm text-customNameBlack">
+            How to Calculate Household Income
+          </h3>
+          <div className="font-bold text-sm text-customStreetcolor text-center">
+            <p>Add up all sources of income, such as:</p>
+            <ol>
+              <li>Job Earnings (salary, wages)</li>
+              <li>Business Income (if you own a business)</li>
+              <li>Investment Income (like interest or dividends)</li>
+            </ol>
+            <div>
+              <p>Example:</p>
+              <ul>
+                <li>Job Earnings: NGN 50,000</li>
+                <li>Business Income: NGN 10,000</li>
+                <li>Investment Income: NGN 5,000</li>
+              </ul>
+              <p>Total Household Income = NGN 65,000</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <button className="bg-customSearchblue px-2 py-4 font-medium text-base">
+            Search
+          </button>
+        </div>
       </div>
     </div>
   );
