79d3f64c44bc751b458e2a4d37edb79b5b834945
kkebuka <kkebuka@gmail.com>
2024-09-16 19:53:48 +0100

    minor change

--- src/pages/landingpage/components/MultiIncomeAdd.jsx
+++ src/pages/landingpage/components/MultiIncomeAdd.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import CancelPresentationIcon from "@mui/icons-material/CancelPresentation";
+import AddIcon from "@mui/icons-material/Add";
 
 function MultiIncomeAdd() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <CancelPresentationIcon />
+      </div>
+      <div>
+        <div>
+          <h2 className="font-bold text-3xl text-customNameBlack">
+            Search better
+          </h2>
+          <p className="font-normal text-base bg-customStreetcolor">
+            Enter requested details to help us provide better search results for
+            your optimized results.
+          </p>
+        </div>
+        <div>
+          <div>
+            <label htmlFor="incomeOne">Income one</label>
+            <input type="number" id="incomeOne" placeholder="Enter in NGN" />
+          </div>
+          <AddIcon />
+          <div>
+            <label htmlFor="incomeTwo">Income one</label>
+            <input type="number" id="incomeTwo" placeholder="Enter in NGN" />
+          </div>
+          <AddIcon />
+          <div>
+            <label htmlFor="incomeThree">Income one</label>
+            <input type="number" id="incomeThree" placeholder="Enter in NGN" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MultiIncomeAdd;
