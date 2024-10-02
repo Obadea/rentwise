4b020ffea6b6b7c4c7bc56bf7a6915569733ae81
kkebuka <kkebuka@gmail.com>
2024-10-02 15:29:48 +0100

    minor change

--- src/pages/propertyPage/components/Address.jsx
+++ src/pages/propertyPage/components/Address.jsx
@@ -3,13 +3,53 @@ import MapIcon from "@mui/icons-material/Map";
 
 function Address() {
   return (
-    <div className="px-4 py-6 lg:p-10 flex flex-col gap-4 mt-20">
-      <div>
+    <div className="px-4 py-6 lg:p-10 flex flex-col  gap-4 mt-16 ">
+      <div className="flex justify-between py-10 border-b-2 border-[#D9D9D9] flex-1 min-w-[45%]">
         <h4 className=" font-medium text-lg text-customdark">Address</h4>
-        <p className="font-bold text-xs">
+        <button className="font-medium text-xs bg-customSearchblue px-2 text-white flex justify-center items-center">
           {" "}
           <MapIcon /> Open on Google Maps
-        </p>
+        </button>
+      </div>
+      <div className="flex flex-col md:flex-row flex-wrap gap-2 md:gap-10 justify-between">
+        <div className="flex justify-between py-6 border-b-2 border-[#D9D9D9] flex-1 min-w-[45%]">
+          <h4 className=" font-medium text-sm text-customdark">Address</h4>
+          <button className="font-normal text-sm   text-customStreetcolor flex justify-center items-center">
+            Victoria Island
+          </button>
+        </div>
+        <div className="flex justify-between py-6 border-b-2 border-[#D9D9D9] flex-1 min-w-[45%]">
+          <h4 className=" font-medium text-sm text-customdark">City</h4>
+          <button className="font-normal text-sm   text-customStreetcolor flex justify-center items-center">
+            Lagos
+          </button>
+        </div>
+        <div className="flex justify-between py-6 border-b-2 border-[#D9D9D9] flex-1 min-w-[45%]">
+          <h4 className=" font-medium text-sm text-customdark">State</h4>
+          <button className="font-normal text-sm   text-customStreetcolor flex justify-center items-center">
+            Lagos
+          </button>
+        </div>
+        <div className="flex justify-between py-6 border-b-2 border-[#D9D9D9] flex-1 min-w-[45%]">
+          <h4 className=" font-medium text-sm text-customdark">
+            Zip/Postal Code
+          </h4>
+          <button className="font-normal text-sm   text-customStreetcolor flex justify-center items-center">
+            10013
+          </button>
+        </div>
+        <div className="flex justify-between py-6 border-b-2 border-[#D9D9D9] flex-1 min-w-[45%]">
+          <h4 className=" font-medium text-sm text-customdark">Area</h4>
+          <button className="font-normal text-sm   text-customStreetcolor flex justify-center items-center">
+            Lagos Island
+          </button>
+        </div>
+        <div className="flex justify-between py-6 border-b-2 border-[#D9D9D9] flex-1 min-w-[45%]">
+          <h4 className=" font-medium text-sm text-customdark">Country</h4>
+          <button className="font-normal text-sm   text-customStreetcolor flex justify-center items-center">
+            Nigeria
+          </button>
+        </div>
       </div>
     </div>
   );
