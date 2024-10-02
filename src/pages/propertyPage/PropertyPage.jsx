78de51344ea4928bea1211dd5911b43342c6d897
kkebuka <kkebuka@gmail.com>
2024-10-02 11:26:05 +0100

    seperated property overview

--- src/pages/propertyPage/PropertyPage.jsx
+++ src/pages/propertyPage/PropertyPage.jsx
@@ -6,12 +6,6 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ShareIcon from "@mui/icons-material/Share";
 import PrintIcon from "@mui/icons-material/Print";
 
-import BedIcon from "@mui/icons-material/Bed";
-import ShowerIcon from "@mui/icons-material/Shower";
-import GarageIcon from "@mui/icons-material/Garage";
-import SquareFootIcon from "@mui/icons-material/SquareFoot";
-import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-
 function PropertyPage() {
   return (
     <div>
@@ -65,54 +59,6 @@ function PropertyPage() {
       </div>
 
       {/* Overview component */}
-      <div>
-        <div>
-          <h4 className="font-medium text-lg">Overview</h4>
-          <p>
-            Property ID: <span>HZ43</span>
-          </p>
-        </div>
-        <div className="flex flex-wrap">
-          <div className="w-[86px]">
-            <h4 className="font-bold text-base text-[#333333]">Apartment</h4>
-            <p className="font-normal text-sm text-[#666666]">Property Type</p>
-          </div>
-          <div className="w-[86px]">
-            <h3 className="font-bold text-base text-[#333333]">
-              <BedIcon className="text-[#5C6872]" /> 3
-            </h3>
-            <p className="font-normal text-sm text-[#666666]">Bedrooms</p>
-          </div>
-          <div className="w-[86px]">
-            <h3 className="font-bold text-base text-[#333333]">
-              <ShowerIcon className="text-[#5C6872]" /> 2
-            </h3>
-            <p className="font-normal text-sm text-[#666666]">Bathrooms</p>
-          </div>
-
-          <div className="w-[86px]">
-            <h3 className="font-bold text-base text-[#333333]">
-              {" "}
-              <GarageIcon className="text-[#5C6872]" /> 3
-            </h3>
-            <p className="font-normal text-sm text-[#666666]">Bedroms</p>
-          </div>
-          <div className="w-[86px]">
-            <h3 className="font-bold text-base text-[#333333]">
-              {" "}
-              <SquareFootIcon className="text-[#5C6872]" /> 2560
-            </h3>
-            <p className="font-normal text-sm text-[#666666]">Bedroms</p>
-          </div>
-          <div className="w-[86px]">
-            <h3 className="font-bold text-base text-[#333333]">
-              {" "}
-              <CalendarMonthIcon className="text-[#5C6872]" /> 2013
-            </h3>
-            <p className="font-normal text-sm text-[#666666]">Bedroms</p>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
