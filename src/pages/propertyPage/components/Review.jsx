25f0ac538092843fdc33ca10a5677e51f3a632cb
kkebuka <kkebuka@gmail.com>
2024-10-07 21:25:12 +0100

    review component

--- src/pages/propertyPage/components/Review.jsx
+++ src/pages/propertyPage/components/Review.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Rating } from "@mui/material";
 
 function Review() {
-  return <div></div>;
+  return (
+    <div className="px-4 py-6 lg:p-10 flex flex-col  gap-4 mt-16 ">
+      <div className="flex flex-col lg:flex-row">
+        <div className="flex">
+          <h3>6 Reviews</h3>
+          <Rating name="read-only" value={3} readOnly />
+          <h3>(3.33 of 5)</h3>
+        </div>
+        <div>
+          Sort by: <span>Default order</span>
+        </div>
+        <div>
+          <div>Leave a Review</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Review;
