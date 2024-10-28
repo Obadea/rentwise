bd9a5fa50b8ed9cba40d86bd3bbb3f9f12960000
kkebuka <kkebuka@gmail.com>
2024-10-28 14:23:23 +0100

    testimonial comment card

--- src/pages/aboutuspage/components/AboutTestimonials.jsx
+++ src/pages/aboutuspage/components/AboutTestimonials.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import AboutBlogPostCard from "./AboutBlogPostCard";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../../landingpage/components/TopProperties";
 import quote from "../../../assets/Open-Quote--Streamline-Ultimate.png.png";
+import testimonial from "../../../assets/testimonials.png";
+
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
 
 function AboutTestimonials() {
+  const [value, setValue] = useState(4);
+
   const settings = {
     lazyLoad: "ondemand",
     dots: true,
@@ -56,7 +62,7 @@ function AboutTestimonials() {
     ],
   };
   return (
-    <div className="px-4 lg:px-24 py-12">
+    <div className="px-4 lg:px-24 py-12 bg-white ">
       <div className="flex flex-col gap-6 ">
         <div className="flex flex-col text-center gap-2">
           <h3 className="font-normal text-4xl text-[#666666] ">TESTIMONIALS</h3>
@@ -64,18 +70,34 @@ function AboutTestimonials() {
             What others says about us
           </p>
         </div>
-        <div className="flex flex-row flex-wrap">
-          <div className="flex flex-col  bg-[#FAFBFF]">
+        <div className="flex flex-row gpa-4 flex-wrap ">
+          <div className="flex flex-col  bg-[#FAFBFF] gap-4 lg:w-[400px] py-8 px-6 rounded-lg shadow-lg">
             <img src={quote} alt="" className="w-[10%]" />
-            <h5 className="font-normal text-base text-[#454F54]">
+            <h5 className="font-normal text-base text-[#454F54] mb-8">
               RentWise made my house-hunting so much easier! I was able to find
               the perfect apartment that fit my budget, thanks to their
               income-based recommendations. No more wasting time on homes I
               couldn’t afford. I felt like they really understood my needs!
             </h5>
             <div className="flex justify-between">
-              <img src="" alt="" />
-              <div className="flex flex-col"></div>
+              <img src={testimonial} alt="woman face" />
+              <div className="flex flex-col justify-between">
+                <h4 className="text-[#454F54] font-semibold text-xl ">
+                  Chinedu
+                </h4>
+                <div className="flex justify-between gap-10 items-center">
+                  <p className="font-normal text-lg text-[#454F54]">
+                    VIctoria Island
+                  </p>
+
+                  <Rating
+                    name="read-only"
+                    value={value}
+                    sx={{ fontSize: 20 }}
+                    readOnly
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
---
+++ src/pages/aboutuspage/components/TestimonialCard.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import quote from "../../../assets/Open-Quote--Streamline-Ultimate.png.png";
+import testimonial from "../../../assets/testimonials.png";
+
+import Rating from "@mui/material/Rating";
+
+function TestimonialCard() {
+  const [value, setValue] = useState(4);
+  return (
+    <div className="flex flex-col  bg-[#FAFBFF] gap-4 lg:w-[400px] py-8 px-6 rounded-lg shadow-lg">
+      <img src={quote} alt="" className="w-[10%]" />
+      <h5 className="font-normal text-base text-[#454F54] mb-8">
+        RentWise made my house-hunting so much easier! I was able to find the
+        perfect apartment that fit my budget, thanks to their income-based
+        recommendations. No more wasting time on homes I couldn’t afford. I felt
+        like they really understood my needs!
+      </h5>
+      <div className="flex justify-between">
+        <img src={testimonial} alt="woman face" />
+        <div className="flex flex-col justify-between">
+          <h4 className="text-[#454F54] font-semibold text-xl ">Chinedu</h4>
+          <div className="flex justify-between gap-10 items-center">
+            <p className="font-normal text-lg text-[#454F54]">
+              VIctoria Island
+            </p>
+
+            <Rating
+              name="read-only"
+              value={value}
+              sx={{ fontSize: 20 }}
+              readOnly
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TestimonialCard;
