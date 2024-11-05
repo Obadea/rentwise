6c7c04b051266e90fdde8970b637cf63b1b36f50
kkebuka <kkebuka@gmail.com>
2024-11-05 12:22:06 +0100

    fix

--- src/pages/aboutuspage/AboutUsPage.jsx
+++ src/pages/aboutuspage/AboutUsPage.jsx
@@ -48,7 +48,7 @@ function AboutUsPage() {
               Lorem ipsum dolor sit amet, consectetur adipisicing elit
             </p>
           </div>
-          <div className="flex  justify-between flex-wrap gap-4 lg:gap-6 md:flex-row  lg:flex-nowrap">
+          <div className="flex  justify-evenly flex-wrap gap-4 lg:gap-6 md:flex-row  lg:flex-nowrap">
             <AboutPhotoCard />
             <AboutPhotoCard />
             <AboutPhotoCard />
