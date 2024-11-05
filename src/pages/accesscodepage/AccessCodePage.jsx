92a0c5857963775e1e3fcd52d2edf5570c8e60ab
kkebuka <kkebuka@gmail.com>
2024-11-05 11:44:33 +0100

    minor change

--- src/pages/accesscodepage/AccessCodePage.jsx
+++ src/pages/accesscodepage/AccessCodePage.jsx
@@ -6,7 +6,7 @@ import MailOutlineIcon from "@mui/icons-material/MailOutline";
 function AccessCodePage() {
   return (
     <div className="flex flex-col lg:flex-row  min-h-screen  ">
-      <div className=" my-4 mx-auto px-3 lg:pl-24 lg:pr-16  lg:w-[50%] ">
+      <div className=" my-4 mx-auto px-4 w-full  lg:pl-24 lg:pr-16 lg:pt-20 lg:w-[50%] ">
         <div className="flex justify-between ">
           <Logo />
           <button className="font-bold text-sm text-customSearchblue lg:hidden">
@@ -32,33 +32,33 @@ function AccessCodePage() {
                 // value={text}
                 // onChange={(event) => setAccessCode(event.target.value)}
                 placeholder="Enter access ID"
-                className="border border-customBlackShade pl-2 py-1"
+                className="border border-customBlackShade text-[#333333] text-2xl pl-2 py-2  "
               />
             </div>
 
             <div className="mt-4 flex flex-col ">
-              <button className="w-full h-[52px] px-[10px] py-[20px] rounded-xl bg-customSearchblue text-white text-base flex justify-center items-center  font-bold ">
+              <button className="w-full h-[52px] px-[10px] py-[20px] rounded-xl bg-customSearchblue text-white text-xl flex justify-center items-center  font-bold ">
                 Continue
               </button>
             </div>
           </form>
-          <div class="container mx-auto text-center my-6">
-            <div class="or-tag relative inline-block mx-4 flex justify-center items-center">
-              <div class="w-1/2 h-1 bg-gray-300"></div>
-              <span class="mx-4 font-normal text-customStreetcolor text-[18px]">
+          <div className="container mx-auto text-center my-6">
+            <div className="or-tag relative  mx-4 flex justify-center items-center">
+              <div className="w-1/2 h-1 bg-gray-300"></div>
+              <span className=" mx-4 font-normal text-customBlackShade text-[18px] text-nowrap">
                 Don’t have access ID?
               </span>
               <div class="w-1/2 h-1 bg-gray-300"></div>
             </div>
           </div>
-          <div className="flex gap-2 justify-around mb-3">
-            <h4 className="flex border gap-3 justify-center items-center border-customBlackShade p-2 text-customStreetcolor font-normal text-base">
-              <WhatsAppIcon />
+          <div className="flex gap-3 flex-col lg:flex-row lg:gap-6 justify-evenly mb-3">
+            <h4 className=" px-5 flex-1 flex border gap-3 justify-center items-center border-customBlackShade p-2 text-customStreetcolor font-normal text-base">
+              <WhatsAppIcon className="text-[#25D366]" />
               Contact through Whatsapp
             </h4>
 
-            <h4 className="flex border items-center gap-2 border-customBlackShade p-2 text-customStreetcolor font-normal text-base">
-              <MailOutlineIcon />
+            <h4 className=" flex-1 flex border px-5 items-center gap-3 border-customBlackShade p-2 text-customStreetcolor font-normal text-base">
+              <MailOutlineIcon className="text-customNameBlack" />
               Send us a mail
             </h4>
           </div>
@@ -72,22 +72,7 @@ function AccessCodePage() {
           backgroundRepeat: "no-repeat",
         }}
         className="hidden lg:block flex-1 relative  "
-      >
-        <div className="absolute w-[70%] rounded-2xl py-4 px-3 gap-4 bottom-14 right-24 bg-[#00000087]">
-          <h4 className="text-textcolor font-bold text-lg">
-            Something to motivate
-          </h4>
-          <p className="font-medium text-sm text-[#FFFFFFA8]">
-            Lorem ipsum dolor sit amet consectetur. Gravida id ut praesent nunc.
-            Sed aliquam mi at urna. Condimentum mi lorem aliquam mattis.
-            Consequat est phasellus velit nisl justo dui. Faucibus dictum
-            habitasse sed vel at nisl non. Nisi placerat purus sed varius
-            euismod. Risus etiam purus donec neque. Non id mi maecenas quam
-            convallis ut varius feugiat iaculis. Quis porttitor urna malesuada
-            mattis in quam. Quam.
-          </p>
-        </div>
-      </div>
+      ></div>
     </div>
   );
 }
