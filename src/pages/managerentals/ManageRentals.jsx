bbd62112131fccfa8f37e335eccc49468d78555f
kkebuka <kkebuka@gmail.com>
2024-11-10 16:54:28 +0100

    manage rentals

---
+++ src/pages/managerentals/ManageRentals.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/Header";
+
+function ManageRentals() {
+  return (
+    <div>
+      <Header newclassName="sticky" className="bg-textcolor" />
+    </div>
+  );
+}
+
+export default ManageRentals;
