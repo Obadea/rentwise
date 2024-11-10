7ea0b30374353b120d82c85657078b469f7ad964
kkebuka <kkebuka@gmail.com>
2024-11-10 17:12:40 +0100

    minor change

--- src/pages/managerentals/ManageRentals.jsx
+++ src/pages/managerentals/ManageRentals.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Header from "../../components/Header";
+import Breadcrumb from "../../components/BreadCrumb";
 
 function ManageRentals() {
   return (
     <div>
       <Header newclassName="sticky" className="bg-textcolor" />
+      <Breadcrumb className="my-12" />
+      <div className="flex flex-col lg:flex-row ">
+        <div className="flex-1">
+          <h3 className="font-bold text-4xl text-customNameBlack">
+            A fast and easy way to manage your rentals seamlessly
+          </h3>
+        </div>
+        <div className="flex-1">
+          <img src="" alt="" className="w-full h-full" />
+        </div>
+      </div>
+      <div className="flex flex-col mx-"></div>
     </div>
   );
 }
