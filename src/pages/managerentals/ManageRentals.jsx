5cadbb5bf6468b1a3732285de74d6e449b9d6f4e
kkebuka <kkebuka@gmail.com>
2024-11-10 17:19:42 +0100

    minor add

--- src/pages/managerentals/ManageRentals.jsx
+++ src/pages/managerentals/ManageRentals.jsx
@@ -7,17 +7,30 @@ function ManageRentals() {
     <div>
       <Header newclassName="sticky" className="bg-textcolor" />
       <Breadcrumb className="my-12" />
-      <div className="flex flex-col lg:flex-row ">
-        <div className="flex-1">
-          <h3 className="font-bold text-4xl text-customNameBlack">
-            A fast and easy way to manage your rentals seamlessly
-          </h3>
+      <div>
+        <div className="flex flex-col lg:flex-row ">
+          <div className="flex-1">
+            <h3 className="font-bold text-4xl text-customNameBlack">
+              A fast and easy way to manage your rentals seamlessly
+            </h3>
+          </div>
+          <div className="flex-1">
+            <img src="" alt="" className="w-full h-full" />
+          </div>
         </div>
-        <div className="flex-1">
-          <img src="" alt="" className="w-full h-full" />
+        <div className="flex flex-col ">
+          <div className="flex flex-col justify-center items-center ">
+            <h4 className="font-bold text-4xl text-customNameBlack">
+              Rent and Manage your Properties
+            </h4>
+            <p className="font-normal text-lg text-customStreetcolor">
+              Increase visibility for your listing, streamline maintenance
+              requests, or manage rent collection seamlessly, we are here to
+              assist you!
+            </p>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col mx-"></div>
     </div>
   );
 }
