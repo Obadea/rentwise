707ae4c9f1b4624e63ad2b05f6b0ec946cf04632
kkebuka <kkebuka@gmail.com>
2024-11-15 16:47:36 +0100

    increase icon size

--- src/pages/notfoundpage/NotFoundPage.jsx
+++ src/pages/notfoundpage/NotFoundPage.jsx
@@ -5,7 +5,7 @@ function NotFoundPage() {
   return (
     <div>
       <div className="flex flex-col justify-center items-center h-screen gap-6">
-        <ErrorIcon sx={{ fontSize: "40px" }} className="text-red-600" />
+        <ErrorIcon sx={{ fontSize: "60px" }} className="text-red-600" />
         <h2 className="text-4xl lg:text-6xl font-bold ">404 Not found</h2>
         <p className="text-customStreetcolor text-lg font-normal">
           This page does not exist.
