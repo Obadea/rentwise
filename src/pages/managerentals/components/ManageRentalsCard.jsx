9d667459acbff95c8f1dbb87797016eb2454cbca
obadea <obadeagbenga@gmail.com>
2025-01-03 14:43:45 +0100

    worked on shortlet payment page

--- src/pages/managerentals/components/ManageRentalsCard.jsx
+++ src/pages/managerentals/components/ManageRentalsCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function ManageRentalsCard({ img, title, description, buttonText }) {
   return (
-    <Card radius="sm" className="pt-8 pb-4">
+    <Card radius="lg" className="pt-8">
       <CardBody className="flex flex-col items-center space-y-3">
         <Image src={img} width="70%" className="m-auto" disableSkeleton />
         <p className="font-bold text-customResultqueryGrey text-[20px] text-center">
@@ -13,8 +13,8 @@ function ManageRentalsCard({ img, title, description, buttonText }) {
           {description}
         </p>
       </CardBody>
-      <CardFooter>
-        <Button color="primary" className="w-full">
+      <CardFooter className="bg-primary p-0">
+        <Button color="primary" className="w-full h-14" radius="none">
           {buttonText}
         </Button>
       </CardFooter>
